ea5051686412aac4a9e10f545630c9bfeec7e82f
Louis-Ghannad-Rezai <hibkoslouisghannad@gmail.com>
2023-04-26 11:58:54 +0200

    change file format + clean code

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 
-import bodyParser from 'body-parser'
 import express from 'express'
 import mongoose  from "mongoose";
 
 let app = express();
 
-//const User = require("../services/user.mjs")
 
-import { getByUserId,getByUserName,generateAuth, isAdmin, getUserByPoliciesId } from './services/user.mjs';
+import { getByUserId,getByUserName,generateAuth, isAdmin, getUserByPoliciesId } from './services/user.js';
 import { getPoliciesByUserName} from './services/policies.js'
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,13 +15,6 @@ await mongoose.connect('mongodb://127.0.0.1:27017/mocky');
 
 
 
-
-// Get user data filtered by user id
-/*
-* Get User data by id or name
-* 
-*
-*/
 router.get("/user", async function(req,res){
 
 if (req.query.id){
@@ -44,9 +35,7 @@ else if (req.query.name){
     let val =  await getUserByPoliciesId(req.query.policiesId)
     res.send(val);
   }}
-}
-
-)
+})
 
 router.post("/auth", async function(req,res){
   let username = req.body.name;
@@ -66,19 +55,11 @@ router.get("/policies", async function(req,res){
     if (req.query.name){
     let val =  await getPoliciesByUserName(req.query.name);
      res.send(val)}
-  // }
-  // else if (req.query.name){
-  //   let val =1// await getUserByPoliciesId(req.query.policiesId);
-  //     res.send(val)
-  //   }
+
    }
 }
 )
 
-
-router.get("/", function (req, res) {
-  res.send("Hello World!");
-});
 
 
 app.listen(3000, function () {
