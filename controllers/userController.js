f6424ad04b0745e378f5c19e90fc393120f83fa6
Louis-Ghannad-Rezai <hibkoslouisghannad@gmail.com>
2023-04-27 18:06:38 +0200

    add controller arch so the index.js file is shorter and clearer

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+
+
+import { getByUserId,getByUserName,  getUserByPoliciesId } from '../services/user.js';
+import { userModel } from '../models/userSchema.js';
+import { policiesModel } from '../models/policiesSchema.js';
+import { getPoliciesByUserName} from '../services/policies.js'
+import {isAdmin,generateAuth} from '../services/utils.js';
+
+export async function  handleUserRequest(req,res){
+    if (req.query.id){
+        let val = await  getByUserId(req?.query?.id);
+          if (val != 1){
+            res.status(200).send(val);
+          } else
+            res.sendStatus(204);
+      }
+      else if (req?.query?.name){
+          let val = await getByUserName(req?.query?.name);
+          if (val != 1){
+            res.status(200).send(val);
+          } else
+            res.sendStatus(204);
+      }
+        else if (req.query.policiesId){
+          if (await isAdmin(req?.query?.token) == 0){
+                res.sendStatus(401);
+          }
+          else {
+            let val =  await getUserByPoliciesId(req?.query?.policiesId);
+          if (val != 1){
+            res.status(200).send(val);
+          } else
+            res.sendStatus(204);
+      }}
+}
--- index.js
+++ index.js
@@ -2,22 +2,18 @@
 import express from 'express'
 import mongoose  from "mongoose";
 import bodyParser from "body-parser"
-import fs from 'fs'
+import {handleUserRequest} from "./controllers/userController.js"
+
+import {generateAuth} from './services/utils.js';
+import { handlePoliciesRequest } from './controllers/policiesController.js';
+
+
+
 let app = express();
-
-
-import { getByUserId,getByUserName,  getUserByPoliciesId } from './services/user.js';
-import { userModel } from './models/userSchema.js';
-import { policiesModel } from './models/policiesSchema.js';
-import { getPoliciesByUserName} from './services/policies.js'
-import {isAdmin,generateAuth} from './services/utils.js';
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var router = express.Router();
-//await mongoose.connect('mongodb://mongo:27017/mocky');
-
 const env = process.env.NODE_ENV || '';
 
 if (env === 'local' || env === 'test') {
@@ -26,43 +22,14 @@ if (env === 'local' || env === 'test') {
 {
   process.env.MONGODB_URI = 'mongodb://mongo:27017/mocky'
 }
-console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI);
-
-
 
 //Router that handle of the request that will return an User or an array of User
 router.get("/user", async function(req,res){
-
-  if (req.query.id){
-    let val = await  getByUserId(req?.query?.id);
-      if (val != 1){
-        res.status(200).send(val);
-      } else
-        res.sendStatus(204);
-  }
-  else if (req?.query?.name){
-      let val = await getByUserName(req?.query?.name);
-      if (val != 1){
-        res.status(200).send(val);
-      } else
-        res.sendStatus(204);
-  }
-    else if (req.query.policiesId){
-      if (await isAdmin(req?.query?.token) == 0){
-            res.sendStatus(401);
-      }
-      else {
-        let val =  await getUserByPoliciesId(req?.query?.policiesId);
-      if (val != 1){
-        res.status(200).send(val);
-      } else
-        res.sendStatus(204);
-  }}
+    handleUserRequest(req,res);
 })
 
 
-//
 router.post("/auth", async function(req,res){
   let username = req?.body?.name;
   let token = await generateAuth(username);
@@ -75,20 +42,8 @@ router.post("/auth", async function(req,res){
 
 
 router.get("/policies", async function(req,res){
-
-  if (await isAdmin(req.query.token) == 0){
-    
-    res.sendStatus(401)
-  }
-  else{
-    if (req.query.name){
-    let val =  await getPoliciesByUserName(req.query.name);
-    res.status(200).send(val)
-    }
-
-   }
-}
-)
+  handlePoliciesRequest(req,res);
+})
 
 app.listen(3000, async function () {
 });
